1c70316cedc2040d956d6b250f6dd39326906ae2
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-10-07 17:51:45 +0530

    program added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Data_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList_Stack demo = new LinkedList_Stack();
+            demo.Add(70);
+            demo.Add(30);
+            demo.Add(56);
+
+            demo.Peek();
+           // demo.Pop();
+            //demo.Pop();
+            //demo.Pop();
+            demo.Pop();
+
+            demo.Display();
+            Console.ReadLine();
+
+            
+        }
+    }
+}
