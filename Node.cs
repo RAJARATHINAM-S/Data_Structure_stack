dc3b1faa4efd8fc187834a9e89039856c574f5d2
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-10-07 16:53:46 +0530

    Linked List Stack Program Added

---
+++ Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structure_Demo
+{
+    class Node
+    {
+
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Data_Structure_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList data = new LinkedList();
+            data.Push(70);
+            data.Push(30);
+            data.Push(56);
+           // data.Display();
+           // data.InsertAtParticularPosition(1, 50);
+            data.Peek();
+           // data.Push(90);
+           
+            data.Pop();
+            data.Display();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
